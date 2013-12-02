b5b344c550ee41d8f6e8bdad0f859da0cba5cb8d
Nikhil Prabhakar <nprabhak@nprabhak-mn.local>
2013-12-02 18:01:36 +0530

    Changes to project.clj for lein 2.

--- project.clj
+++ project.clj
@@ -4,4 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :repl-options {:port 4001})
+
+  ;; For lein < 1.7.0
+  ;; https://github.com/technomancy/swank-clojure/tree/master/lein-swank
+  ;; :dev-dependencies [[swank-clojure "1.4.0"]]
+
+  ;; nrepl
+  ;; :repl-options {:port 4001}
+
+  ;; For lein >= 1.7.0
+  ;; https://github.com/technomancy/swank-clojure/tree/master/lein-swank
+  :plugins [[lein-swank "1.4.5"]]
+  )
